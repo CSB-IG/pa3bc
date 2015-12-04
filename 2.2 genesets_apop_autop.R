e72290b192e4faebe14e95c4a0a408891c051133
Angel García-Campos <xxwildfangxx@gmail.com>
2015-12-03 21:14:32 -0600

    Update 2.2 genesets_apop_autop.R
    Updates to write apoptosis and autophagy genesets in GMT format

--- 2.2 genesets_apop_autop.R
+++ 2.2 genesets_apop_autop.R
@@ -14,7 +14,7 @@ apop_autop <- read.delim(file = "GO.apoptotic_process+autophagy.txt", as.is = T,
 # Extract genesets from Bader et al., GODB
 GODB_apop_autop <- GODB[apop_autop[,1],]
 
-# Wipeout blanck columns (non-found: no genes annotated), add NaNs in 2nd column
+# Wipeout blank columns (non-found: no genes annotated), add NaNs in 2nd column
 m <- na.omit(GODB_apop_autop)
 m[,2] <- "NaN"
 m <- as.matrix(m)
@@ -23,9 +23,50 @@ m <- as.matrix(m)
 write.table(x = m, file = "genesets_apop_autop.txt", row.names = TRUE, 
             col.names = F, sep = "\t")
 
-
 # Write genesets to GMT file (for use in GSEA and PATHIFIER)
 pathifier_labels <- paste(rownames(m), m[,1],sep = "_")
 m[,1] <- pathifier_labels
 write.table(x = m, file = "PATHIFIER_genesets_apop_autop.gmt", row.names = F, 
+            col.names = F, sep = "\t", quote = FALSE)
+
+################################################################################
+# Only Apoptosis genesets
+################################################################################
+# Read selected apoptosis and autophagy GO ids
+apop_autop <- read.delim(file = "GO.apoptotic_process_WITHOUT_ctsapop.txt", 
+                         as.is = T, header = FALSE)
+
+# Extract genesets from Bader et al., GODB
+GODB_apop_autop <- GODB[apop_autop[,1],]
+
+# Wipeout blanck columns (non-found: no genes annotated), add NaNs in 2nd column
+m <- na.omit(GODB_apop_autop)
+m[,2] <- "NaN"
+m <- as.matrix(m)
+
+# Write genesets to GMT file (for use in GSEA and PATHIFIER)
+pathifier_labels <- paste(rownames(m), m[,1],sep = "_")
+m[,1] <- pathifier_labels
+write.table(x = m, file = "PATHIFIER_genesets_apop.gmt", row.names = F, 
+            col.names = F, sep = "\t", quote = FALSE)
+
+################################################################################
+# Only autophagy genesets
+################################################################################
+# Read selected apoptosis and autophagy GO ids
+apop_autop <- read.delim(file = "GO.autophagy.txt", as.is = T,
+                         header = FALSE)
+
+# Extract genesets from Bader et al., GODB
+GODB_apop_autop <- GODB[apop_autop[,1],]
+
+# Wipeout blanck columns (non-found: no genes annotated), add NaNs in 2nd column
+m <- na.omit(GODB_apop_autop)
+m[,2] <- "NaN"
+m <- as.matrix(m)
+
+# Write genesets to GMT file (for use in GSEA and PATHIFIER)
+pathifier_labels <- paste(rownames(m), m[,1],sep = "_")
+m[,1] <- pathifier_labels
+write.table(x = m, file = "PATHIFIER_genesets_autop.gmt", row.names = F, 
             col.names = F, sep = "\t", quote = FALSE)
